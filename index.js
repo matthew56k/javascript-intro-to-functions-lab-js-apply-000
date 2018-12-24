a350b16b6f34f41689656c7fa634f7c2239a3634
kind-karma-7725 <mattehowe@gmail.com>
2018-12-24 18:43:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ const logShout = str => console.log(str.toUpperCase());
 const logWhisper = str => console.log(str.toLowerCase());
 
 const sayHiToGrandma = str => {
+<<<<<<< HEAD
   if (str === str.toLowerCase()) {
     return `I can't hear you!`
   };
   if (str === str.toUpperCase()) {
+=======
+  if (str === lowercase) {
+    return `I can't hear you!`
+  };
+  if (str === uppercase) {
+>>>>>>> f0dfc8d798c417303a7f41e6690fc27f3fbcb929
     return `YES INDEED!`
   };
   if (str === `I love you, Grandma.`) {
